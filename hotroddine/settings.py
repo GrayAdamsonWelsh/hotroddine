93091c4596d721a32a8fe9dd67652c3e442837ef
Graeme Adamson <graya0277@gmail.com>
2024-03-13 11:42:29 +0000

    Added app for testing for intial deploy

--- hotroddine/settings.py
+++ hotroddine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kxr1hfb55p-wo^=%bz$_qzsq$7%hh-nt3mqmrcklhbs7kw$^++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-grayadamsonw-hotroddine-d0vx267t7po.ws-eu109.gitpod.io']
+ALLOWED_HOSTS = ['8000-grayadamsonw-hotroddine-d0vx267t7po.ws-eu109.gitpod.io', '.herokuapp.com' ]
 
 
 # Application definition
